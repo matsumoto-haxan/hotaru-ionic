47ff272253a52fb6b8aae7d946201b15dfdf5df0
matsumoto-haxan <matsumoto@haxan.co.jp>
2019-08-02 16:52:17 +0900

    周りのユーザのアイコンを表示

--- hotaru-ionic/src/app/tab2/tab2.page.ts
+++ hotaru-ionic/src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { Map, latLng, tileLayer, Layer, marker, icon } from 'leaflet';
+import { Map, latLng, tileLayer, Layer, marker, icon, popup } from 'leaflet';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 // import { Geolocation } from 'ionic-native';
 import { GeoCrudService } from './../service/geo-crud.service';
@@ -41,65 +41,13 @@ export class Tab2Page {
   lastUpdatePosition: number[];
   range = 0.0001; // 移動基準距離（20mくらい）
 
+/**
+ * ◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾ 使うメソッド ◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾
+ */
 
-  setGeoRecord() {
-    const gfx = geofirex.init(firebase);
-  }
-
-  testGeoFire() {
-    this.addGeoFire([35, 135]);
-  }
-
-  addGeoFire(geo: number[]) {
-    const gfx = geofirex.init(firebase);
-    const ul = gfx.collection('Locations');
-    const point = gfx.point(geo[0], geo[1]);
-    ul.add({
-      uid: this.userId,
-      timestamp: new Date(),
-      position: point.data
-    }).then((elm) => {
-      this.currentRecordId = elm.id;
-    });
-  }
-
-  updateGeoFire(currentID: string, geo: number[]) {
-    const gfx = geofirex.init(firebase);
-    const ul = gfx.collection('Locations');
-    const point = gfx.point(geo[0], geo[1]);
-    ul.setPoint(currentID, 'position', geo[0], geo[1]);
-  }
-
-  testGetGeoFire() {
-    const gfx = geofirex.init(firebase);
-    const ul = gfx.collection('Locations');
-    const center = gfx.point(40.1, -119.1);
-    const radius = 100;
-    const field = 'position';
-    const query = ul.within(center, radius, field);
-
-    query.subscribe((data) => {
-      alert(
-        data[0].name
-      );
-    });
-  }
-
-
-  obtenerPosicion() {
-    this.geolocation.getCurrentPosition().then(resp => {
-      this.currentPosition = [resp.coords.latitude, resp.coords.longitude];
-      this.createMap(this.currentPosition);
-    })
-      .catch(error => {
-        console.log('obtenerPosicion():' + error.message);
-        alert('obtenerPosicion():' + error.message);
-      });
-  }
-
-  /**
-   * 画面遷移してきた時に動作する
-   */
+/**
+ * 画面遷移してきた時に動作する
+ */
   ionViewDidEnter() {
 
     // userIdがカラなら取得
@@ -111,26 +59,10 @@ export class Tab2Page {
       this.navCtrl.navigateRoot('signin');
     }
 
-    // GeoFirex用のDB参照を設定
-
-    // this.platform.ready().then(() => this.obtenerPosicion());
-
-    /*
-    const options = {
-      timeout: 3000
-    };
-    this.geolocation.getCurrentPosition(options).then((resp) => {
-      this.currentPosition = [resp.coords.latitude, resp.coords.longitude];
-      this.createMap(this.currentPosition);
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
-    */
-
-    // マップが用意されていなければ生成する（位置情報が手に入らないので、適当に日本標準時）
+    // マップが用意されていなければ生成する（位置情報が手に入らない場合、適当に新宿駅）
     if (!this.map) {
       if (!this.currentPosition) {
-        this.currentPosition = [35, 135];
+        this.currentPosition = [35.6940631, 139.7099038];
       }
       this.map = new Map('mapId').setView(this.currentPosition, 17);
       tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
@@ -138,126 +70,85 @@ export class Tab2Page {
         attribution: 'OSM © CartoDB',
       }).addTo(this.map);
 
-      // this.addMarker(geo);
+      // 自分のアイコンを作成
       const myIcon = icon({
         iconUrl: 'assets/imgs/kurousa.png',
         iconSize: [100, 100],
-        iconAnchor: [100, 100],
+        iconAnchor: [50, 100],
         // popupAnchor: [-3, -76],
         // shadowUrl: 'my-icon-shadow.png',
         // shadowSize: [68, 95],
         // shadowAnchor: [22, 94]
       });
 
-      this.myMarker = marker(this.currentPosition, { icon: myIcon }).addTo(this.map)
-        // .bindPopup('ここにいる')
-        .openPopup();
+      this.myMarker = marker(this.currentPosition, { icon: myIcon }).addTo(this.map);
     }
 
-    // 位置情報を追跡して、変化があったら動作する
     if (!this.watch) {
-      this.watch = this.geolocation.watchPosition();
-      this.subscription = this.watch.subscribe((data) => {
-        this.currentPosition = [data.coords.latitude, data.coords.longitude];
-        // alert('変化あり: ' + this.currentPosition);
-
-        const latlng = new latLng(this.currentPosition[0], this.currentPosition[1]);
-        this.myMarker.setLatLng(latlng);
-
-        this.updateGeoRecord(this.currentRecordId, this.currentPosition);
-      });
+      this.setTracking();
     }
   }
 
-  createMap(geo: number[]) {
-    // In setView add latLng and zoom
-    this.map = new Map('mapId').setView(geo, 17);
-    tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      attribution: 'OSM © CartoDB',
-    }).addTo(this.map);
+  /**
+   * 自分の位置情報をトラッキングしてマップを更新する
+   * TODO: バックグラウンド取得できるようにする
+   */
+  setTracking() {
+    this.watch = this.geolocation.watchPosition();
+    this.subscription = this.watch.subscribe((data) => {
+      this.currentPosition = [data.coords.latitude, data.coords.longitude];
 
-    // this.addMarker(geo);
-    this.myMarker = marker(geo).addTo(this.map)
-      .bindPopup('ここにいる')
-      .openPopup();
+      const latlng = new latLng(this.currentPosition[0], this.currentPosition[1]);
+      const panopt = { animate: true };
+      this.map.panTo(latlng, panopt);
+      this.myMarker.setLatLng(latlng);
+
+      this.updateGeoRecord(this.currentRecordId, this.currentPosition);
+    });
   }
 
-  addMarker(geo: number[]) {
-    this.myMarker = marker(geo);
-    this.myMarker.bindPopup('<p>Tashi Delek - Bangalore.</p>');
-    // this.map.addLayer(markPoint);
-    this.myMarker.addTo(this.map);
-  }
-
-  testMoveMarker() {
-    const latlng = new latLng(this.currentPosition[0] + 0.0001, this.currentPosition[1] + 0.0001);
-    this.myMarker.setLatLng(latlng);
-  }
-
-  setSampleMarker() {
-    alert('setSampleMarker()');
-    this.addMarker([35.666, 139.765]);
-  }
-
-  ionViewWillLeave() {
-    // this.subscription.unsubscribe();
-    // this.map.remove();
-  }
-
-  savegeo() {
-    alert(this.currentPosition);
-    this.createGeoRecord(this.userId, this.currentPosition);
-  }
-
-/**
- * 自分の位置情報をクラウドに保存する
- * @param uid string
- * @param geo number[]
- */
-  createGeoRecord(uid: string, geo: number[]) {
-    const record = {};
-    record['uid'] = uid;
-    record['latitude'] = geo[0];
-    record['longitude'] = geo[1];
-    record['timestamp'] =  this.getCurrentTime();
-    this.geoClud.create_NewGeo(record).then(resp => {
-      // 自分のレコードIDをローカルに保存
-      this.currentRecordId = resp.id;
-
+  /**
+   * 自分の位置情報をクラウドに保存する
+   * @param geo number[]
+   */
+  createGeoRecord(userid: string, geo: number[]) {
+    const gfx = geofirex.init(firebase);
+    const ul = gfx.collection('Locations');
+    const point = gfx.point(geo[0], geo[1]);
+    ul.add({
+      uid: userid,
+      timestamp: Date.parse(new Date().toString()),
+      position: point.data
+    }).then((elm) => {
+      // 自分のレコードIDをローカル保存
+      this.currentRecordId = elm.id;
       // 最終更新位置を保存
       this.lastUpdatePosition = geo;
-      // alert('位置情報をクラウドに新規作成');
-    })
-      .catch(error => {
-        console.log(error);
-      });
+    });
   }
 
   /**
    * 自分の位置情報をクラウドに更新する
-   * @param currentId 
-   * @param geo 
+   * @param currentId string
+   * @param geo number[]
    */
   updateGeoRecord(currentId: string, geo: number[]) {
     if (currentId) {
       // 既にレコードがある場合
-      const abLat = Math.abs(geo[0] - this.lastUpdatePosition[0]);
-      const abLng = Math.abs(geo[1] - this.lastUpdatePosition[1]);
 
-      // クラウド保存の値から20m以上動いていたらクラウドにアップデート
-      if (abLat > this.range && abLng > this.range) {
-        const record = {};
-        record['latitude'] = geo[0];
-        record['longitude'] = geo[1];
-        record['timestamp'] = this.getCurrentTime();
-        this.geoClud.update_Geo(currentId, record);
+      if (this.isMoved(geo, this.lastUpdatePosition)) {
+        const gfx = geofirex.init(firebase);
+        const ul = gfx.collection('Locations');
+        const point = gfx.point(geo[0], geo[1]);
+        // ul.setPoint(currentID, 'position', geo[0], geo[1]);
+        ul.setDoc(currentId, {
+          timestamp: Date.parse(new Date().toString()),
+          position: point.data
+        });
 
         // 最終更新位置を更新
         this.lastUpdatePosition = geo;
-        // alert('位置情報をクラウド更新');
       } else {
-        // alert('近距離なのでクラウド更新しません');
       }
 
     } else {
@@ -267,18 +158,113 @@ export class Tab2Page {
   }
 
   /**
-   * 現在時刻をタイムスタンプで取得
-   * TODO: FB側でタイムスタンプだと認識してくれないので形式は要検討
+   * ２地点の距離が規定値以上離れているかチェックする
+   * @param geo1 number[]
+   * @param geo2 number[]
+   * @returns boolean
    */
-  getCurrentTime() {
-    const d = new Date();
-    const year = d.getFullYear();
-    const month = d.getMonth() + 1;
-    const day = d.getDate();
-    const hour = (d.getHours() < 10) ? '0' + d.getHours() : d.getHours();
-    const min = (d.getMinutes() < 10) ? '0' + d.getMinutes() : d.getMinutes();
-    const sec = (d.getSeconds() < 10) ? '0' + d.getSeconds() : d.getSeconds();
-    return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec;
+  isMoved(geo1: number[], geo2: number[]) {
+    const abLat = Math.abs(geo1[0] - geo2[0]);
+    const abLng = Math.abs(geo1[1] - geo2[1]);
+    return abLat > this.range && abLng > this.range;
+  }
+
+  /**
+   * 画面遷移して出るときに動作
+   */
+  ionViewWillLeave() {
+    // this.subscription.unsubscribe();
+    // this.map.remove();
+  }
+
+/**
+ * ◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾ テスト用のメソッド ◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾◾
+ */
+
+  testGetGeoFire() {
+    const gfx = geofirex.init(firebase);
+    const ul = gfx.collection('Locations');
+    const center = gfx.point(35.6658834, 139.7656723,);
+    const radius = 100;
+    const field = 'position';
+    const query = ul.within(center, radius, field);
+
+    query.subscribe((data) => {
+      alert(
+        data[0].uid
+      );
+    });
+  }
+
+  /**
+   * 近くのユーザ情報を取得する
+   */
+  getNearUser(centerGeo: number[], radius: number) {
+    const dt = new Date();
+    const startDate = dt.setMinutes(dt.getMinutes() - 30).toString();
+    const field = 'position';
+    const gfx = geofirex.init(firebase);
+    // TODO: 範囲検索に他の条件を追加する方法がよくわからないです
+    // const ul = gfx.collection('Locations', ref => {
+    //   return ref.where('timestamp', '>=', '0');
+    // });
+    const ul = gfx.collection('Locations');
+    const center = gfx.point(centerGeo[0], centerGeo[1]);
+    const query = ul.within(center, radius, field);
+    query.subscribe((data) => {
+      data.forEach(elm => {
+        if (elm.uid !== this.userId) {
+          this.setMarker(elm);
+        }
+      });
+
+    });
+  }
+
+  setMarker(elm: any) {
+
+    const elmGeo = [elm.position.geopoint.latitude, elm.position.geopoint.longitude];
+
+    // マーカー
+    const uIcon = icon({
+      iconUrl: '',
+      iconSize: [10, 10],
+      iconAnchor: [10, 10],
+      shadowUrl: '',
+      shadowSize: [0, 0],
+      shadowAnchor: [0, 0]
+
+    });
+    const mkOption = {
+      icon: uIcon
+    };
+    const newMarker = marker(mkOption);
+    newMarker
+      .setLatLng(latLng(elmGeo[0], elmGeo[1]))
+      .bindPopup(
+      '<p>uid:' + elm.uid + '</p>' +
+      '<p>timestamp:' + elm.timestamp + '</p>'
+     );
+    newMarker.addTo(this.map);
+
+    // ポップアップウインドウ
+    const popOption = {
+      minWidth: 100,
+      autoPanPaddingTopLeft: [10, 10]
+    };
+    const newPopup = popup(popOption);
+    newPopup
+      .setLatLng(latLng(elmGeo[0], elmGeo[1]))
+      .setContent(
+        '<p>uid:' + elm.uid + '</p>' +
+        '<p>timestamp:' + elm.timestamp + '</p>'
+      );
+  }
+
+  testGetNear() {
+    this.getNearUser(this.currentPosition, 100);
+    // this.testGetGeoFire();
+
   }
 
 
